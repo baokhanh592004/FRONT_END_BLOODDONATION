509c6332dcb6b48057d9398e728eed85a01d7813
baokhanh592004 <baokhanh21qb@gmail.com>
2025-06-08 22:00:19 +0700

    feat(staff): Implement full staff module UI and routing

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import HomePages from '../pages/HomePages';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
+// Layouts
 import AppLayout from '../layouts/AppLayout';
-import Dashboard from '../pages/Dashboard';
+import StaffLayout from '../layouts/StaffLayout'; // <-- IMPORT
+
+// General Pages
+import HomePages from '../pages/HomePages';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
-import StaffDashboard from '../pages/StaffDashboard'; // Import trang Dashboard của nhân viên
+import ForgotPasswordPage from '../pages/ForgotPasswordPage';
 
-import StaffRoute from './StaffRouter'; // Import StaffRoute để bảo vệ route staff
+// Staff Pages
+import StaffDashboard from '../pages/staff/StaffDashboard';
+import PatientManagementPage from '../pages/staff/PatientManagementPage';
+import DonationUpdatePage from '../pages/staff/DonationUpdatePage';
+import BloodInventoryPage from '../pages/staff/BloodInventoryPage';
+import UrgentDonorSearchPage from '../pages/staff/UrgentDonorSearchPage';
+import RequestApprovalPage from '../pages/staff/RequestApprovalPage';
+
+// Route Protector
+import StaffRoute from './StaffRoute';
+
 export default function AppRouters() {
   return (
     <Routes>
+      {/* Routes cho người dùng chung */}
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePages />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-
-        <Route path="/staff/dashboard" element= {
-        <StaffRoute>
-          {/* Chỉ nhân viên mới có thể truy cập trang này */}
-          {/* Nếu không có quyền, sẽ chuyển hướng về trang đăng nhập */}
-          <StaffDashboard />
-        </StaffRoute>
-        }/>
       </Route>
-      
+
+      {/* Routes được bảo vệ cho Staff */}
+      <Route
+        path="/staff"
+        element={
+          <StaffRoute>
+            <StaffLayout />
+          </StaffRoute>
+        }
+      >
+        <Route path="dashboard" element={<StaffDashboard />} />
+        <Route path="patients" element={<PatientManagementPage />} />
+        <Route path="donations" element={<DonationUpdatePage />} />
+        <Route path="inventory" element={<BloodInventoryPage />} />
+        <Route path="search-urgent" element={<UrgentDonorSearchPage />} />
+        <Route path="requests" element={<RequestApprovalPage />} />
+      </Route>
     </Routes>
   );
 }
