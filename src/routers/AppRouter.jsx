646b3b849652add6f1cb1ea6482ac3353eac9247
2amateur <ntantrong9.2@gmail.com>
2025-06-19 07:46:36 +0700

    Change approuter to be the same as main

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,24 +1,62 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePages from '../pages/HomePages';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
+
+// Layouts
 import AppLayout from '../layouts/AppLayout';
-import Dashboard from '../pages/Dashboard';
+import StaffLayout from '../layouts/StaffLayout'; // <-- IMPORT
+
+// General Pages
+import HomePages from '../pages/HomePages';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
-import Profile from '../pages/Profile';
+import ForgotPasswordPage from '../pages/ForgotPasswordPage';
+import DonationRegistrationPage from '../pages/member/DonationRegistrationPage'; // <-- THÊM MỚI
+import DonationQuestionnairePage from '../pages/member/DonationQuestionnairePage'; // <-- THÊM MỚI
+// Staff Pages
+import StaffDashboard from '../pages/staff/StaffDashboard';
+import DonationManagementPage from '../pages/staff/DonationManagementPage'; // THAY THẾ PatientManagementPage
+import DonorHealthCheckPage from '../pages/staff/DonorHealthCheckPage'; // <-- THÊM MỚI
+
+
+import BloodInventoryPage from '../pages/staff/BloodInventoryPage';
+import UrgentDonorSearchPage from '../pages/staff/UrgentDonorSearchPage';
+import RequestApprovalPage from '../pages/staff/RequestApprovalPage';
+
+// Route Protector
+import StaffRoute from './StaffRoute';
+
 export default function AppRouters() {
   return (
     <Routes>
+      {/* Routes cho người dùng chung */}
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePages />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
+
+        {/* === SỬ DỤNG ROUTE MỚI === */}
+        <Route path="/register-donation" element={<DonationRegistrationPage />} />
+        <Route path="/member/donation-questionnaire" element={<DonationQuestionnairePage />} />
       </Route>
-      
+
+      {/* Routes được bảo vệ cho Staff */}
+      <Route
+        path="/staff"
+        element={
+          <StaffRoute>
+            <StaffLayout />
+          </StaffRoute>
+        }
+      >
+        <Route path="dashboard" element={<StaffDashboard />} />
+          {/* PHẦN NÀY LẤY ID TỪ BACKEND LÊN */}
+          <Route path="donors" element={<DonationManagementPage />} /> {/* Route cho danh sách */}
+          <Route path="donors/:donorId" element={<DonorHealthCheckPage />} /> {/* Route cho trang chi tiết/khai báo */} 
+        <Route path="inventory" element={<BloodInventoryPage />} />
+        <Route path="search-urgent" element={<UrgentDonorSearchPage />} />
+        <Route path="requests" element={<RequestApprovalPage />} />
+      </Route>
     </Routes>
   );
 }
