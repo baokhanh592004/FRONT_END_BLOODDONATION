5a531c6832156f8a5721ae6fdb691b4078372bd9
2amateur <ntantrong9.2@gmail.com>
2025-07-17 00:02:29 +0700

    add head column that missing in the table

--- src/pages/member/DonationHistoryPage.jsx
+++ src/pages/member/DonationHistoryPage.jsx
@@ -172,6 +172,9 @@ const DonationHistoryPage = () => {
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Cơ sở y tế
                   </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Trạng thái
+                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
