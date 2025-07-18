b3e7b0f92b345dae955ad0134b73945cd219d2d6
Luong <luongdz10vnvt@gmail.com>
2025-07-19 04:28:04 +0700

    fix axiosClient

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -11,12 +11,8 @@ import axios from 'axios';
 // Đọc biến môi trường với tiền tố VITE_
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // Vite dùng `import.meta.env`
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
-// ✅ Gắn token vào tất cả request tự động
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
